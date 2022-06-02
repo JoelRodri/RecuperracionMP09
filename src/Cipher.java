1549dfc3fc93097a4c4795b918f8bce4ecf9a5e7
JoelRope <jrdori@elpuig.xeill.net>
2022-06-02 14:43:36 +0200

    terminado

---
+++ src/Cipher.java
@@ -0,0 +1,13 @@
+
+
+public class Cipher {
+    public static void main(String[] args) {
+        Metodos.ej1();
+        Metodos.ej2();
+        Metodos.ej3();
+        Metodos.ej4();
+        Metodos.ej5();
+        Metodos.ej6();
+        Metodos.ej7();
+    }
+}
